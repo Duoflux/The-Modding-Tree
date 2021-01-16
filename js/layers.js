b475e23a1e9ba8308289f1fba016fb552cb2592f
Duoflux <77508260+Duoflux@users.noreply.github.com>
2021-01-15 18:33:31 -0600

    Update layers.js
    Let's hope this fixes things

--- js/layers.js
+++ js/layers.js
@@ -23,9 +23,10 @@ addLayer("c", {
         11: {
             title: "Copper", // Optional, displayed at the top in a larger font
             cost(x=player[this.layer].buyables[this.id]) { // cost for buying xth buyable, can be an object if there are multiple currencies
-                if (x.gte(25)) x = x.pow(2).div(25)
-                let cost = Decimal(10)
-                return cost.floor()
+                if (getBuyableAmount(this.layer, this.id).lt(51)) return new Decimal(10)
+                if (x.gte(51)) x = x.pow(2).div(25)
+                let cost = Decimal.pow(2, x.pow(1.5))
+                    return cost.floor()
             },
             effect(x=player[this.layer].buyables[this.id]) { // Effects of owning x of the items, x is a decimal
                 let eff = {}
