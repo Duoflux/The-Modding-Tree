9af1cb9ba46e5e2bb9f7aa4cea10f523df72d691
Duoflux <77508260+Duoflux@users.noreply.github.com>
2021-01-16 01:30:15 -0600

    Update layers.js
    Getting the obvious (if less likely) culprit fixed first.

--- js/layers.js
+++ js/layers.js
@@ -103,9 +103,9 @@ addLayer("c", {
         },
     },
 })
-addLayer("R1", {
-    layer: "r1", // This is assigned automatically, both to the layer and all upgrades, etc. Shown here so you know about it
-    name: "rank1", // This is optional, only used in a few places, If absent it just uses the layer id.
+addLayer("Fi", {
+    layer: "fi", // This is assigned automatically, both to the layer and all upgrades, etc. Shown here so you know about it
+    name: "first rank", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "Fi", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
