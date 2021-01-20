91a521a50f568a9f7ea6b31bd18867485b165980
Duoflux <77508260+Duoflux@users.noreply.github.com>
2021-01-20 11:50:31 -0600

    Update layers.js
    Let's see if this part works before going too crazy.

--- js/layers.js
+++ js/layers.js
@@ -136,6 +136,7 @@ addLayer("cu", {
             effect(x=player[this.layer].buyables[this.id]) { // Effects of owning x of the items, x is a decimal
                 let eff = {}
                 eff.first = Decimal.add(1)
+                player[this.layer].total = player[this.layer].total.add(1)
             
                 if (x.gte(51)) eff.second = x.pow(0.8)
                 else eff.second = x.times(-1).pow(0.8).times(-1)
