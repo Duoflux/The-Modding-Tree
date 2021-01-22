ff1efe0a52bd67d85d2956f7d7bee6b151856340
Duoflux <77508260+Duoflux@users.noreply.github.com>
2021-01-22 13:20:45 -0600

    Update layers.js
    More balancing

--- js/layers.js
+++ js/layers.js
@@ -15,7 +15,7 @@ addLayer("c", {
     baseResource: "fame", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.75, // Prestige currency exponent
     row: 0, // Row the layer is in on the tree (0 is the first row)
     buyables: {
         rows: 7,
