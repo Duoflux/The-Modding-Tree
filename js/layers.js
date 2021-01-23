285b239a349dadacf55ccdc78daf9fa02d395d63
Duoflux <77508260+Duoflux@users.noreply.github.com>
2021-01-22 23:37:21 -0600

    Update layers.js
    Needs more balancing!

--- js/layers.js
+++ js/layers.js
@@ -253,14 +253,14 @@ addLayer("m", {
         12: {
             title: "More Racks",
             description: "Double base fame gain.",
-            cost: new Decimal(5),
+            cost: new Decimal(3),
             unlocked() {return hasUpgrade(this.layer, 11)},
             effect: 2
         },
         13: {
             title: "Word-of-Mouth Advertising",
             description: "Fame gain increases based on fame.",
-            cost: new Decimal(30),
+            cost: new Decimal(5),
             unlocked() {return hasUpgrade(this.layer, 12)},
             effect() {
                 let wom = player.points.add(1).max(1)
