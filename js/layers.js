8a7411d3dffd1793b059471d667c83c21c834949
Duoflux <77508260+Duoflux@users.noreply.github.com>
2021-01-23 19:15:18 -0600

    Update layers.js
    cleaning code

--- js/layers.js
+++ js/layers.js
@@ -133,10 +133,6 @@ addLayer("f", {
             effect(x=player[this.layer].buyables[this.id]) { // Effects of owning x of the items, x is a decimal
                 let eff = {}
                 eff.first = Decimal.add(1)
-            
-                if (x.gte(51)) eff.second = x.pow(0.8)
-                else eff.second = x.times(-1).pow(0.8).times(-1)
-                return eff;
             },
             display(x=player[this.layer].buyables[this.id]) { // Everything else displayed in the buyable button after the title
                 let data = tmp[this.layer].buyables[this.id]
@@ -182,10 +178,6 @@ addLayer("f", {
             effect(x=player[this.layer].buyables[this.id]) { // Effects of owning x of the items, x is a decimal
                 let eff = {}
                 eff.first = Decimal.add(1)
-            
-                if (x.gte(51)) eff.second = x.pow(0.8)
-                else eff.second = x.times(-1).pow(0.8).times(-1)
-                return eff;
             },
             display(x=player[this.layer].buyables[this.id]) { // Everything else displayed in the buyable button after the title
                 let data = tmp[this.layer].buyables[this.id]
