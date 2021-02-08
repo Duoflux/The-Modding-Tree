d863fa97cee6d6d4623ca71f5a290d110b14d845
Duoflux <77508260+Duoflux@users.noreply.github.com>
2021-02-08 14:03:18 -0600

    Update layers.js
    whoopsie

--- js/layers.js
+++ js/layers.js
@@ -123,7 +123,7 @@ addLayer("f", {
     },
     resource: "crafted swords", // Name of prestige currency
     baseResource: "coins", // Name of resource prestige is based on
-    baseAmount() {return new Decimal(player.f.points)}, // Get the current amount of baseResource
+    baseAmount() {return new Decimal(player.v.points)}, // Get the current amount of baseResource
     exponent: 0.5, // Prestige currency exponent
     row: 1, // Row the layer is in on the tree (0 is the first row)
     buyables: {
